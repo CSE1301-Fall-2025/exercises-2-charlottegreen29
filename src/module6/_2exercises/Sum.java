a4bb0c47e5c32178a9f3f8e8cb9d158d5df25932
charlottegreen29 <c.j.green@wustl.edu>
2025-10-27 11:14:14 -0500

    finally figured out helper methods!

--- src/module6/_2exercises/Sum.java
+++ src/module6/_2exercises/Sum.java
@@ -2,15 +2,29 @@ package module6._2exercises;
 
 public class Sum {
 
-	// public static double sum(double[] array){
-	// 	if (array.length==0){
-	// 		return 0;
-	// 	}
-	// 	else{
-	// 		sumHelper(array);
-	// 		return 0; // not done yet
-	// 	}
-	// }
+	public static double sumLoop(double[] array){  //okay so I got it to work as a for loop; now let's do it with recursion
+		double sum = 0;
+		for (int i=0; i<array.length; i++){
+			sum+=array[i];
+		}
+		return sum;
+	}
+
+	public static double sum(double[] array){
+		double sum = 0.0;
+		int i = array.length-1;
+		sum = sumHelper(array, sum, i);
+		return sum;
+	}
+
+	public static double sumHelper(double[] array, double sum, int i){
+		if (i<0){
+			return sum;
+		}
+		else {
+			return array[i] + sumHelper(array,sum,i-1);
+		}
+	}
 
 	// public static double[] sumHelpler(double[] array){
 	// 	double[] newArray = new double[array.length-1];
@@ -19,6 +33,8 @@ public class Sum {
 	// }
 
 	public static void main(String[] args) {
-		//test your method here
+		double[] lesMisArray = {2,4,6,0,1};
+		System.out.println(sumLoop(lesMisArray));
+		System.out.println(sum(lesMisArray));
 	}
 }
